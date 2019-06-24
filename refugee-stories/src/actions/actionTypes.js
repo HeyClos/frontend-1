e08812b747352ac65ad341b24bbc10e98a5dd27d
Hoxtygen <hoxtygen@live.com>
2019-06-24 23:24:05 +0100

    Add redux action types
    All actions that needed to be carried out requires that redux action be created for it. Actions like login, logout, getpost..etc  will require correponding redux action, and for each action there must be an action type. The action type is what's fleshed out in this commit.

---
+++ refugee-stories/src/actions/actionTypes.js
@@ -0,0 +1,34 @@
+// All posts
+export const  FETCH_POSTS = "FETCH_POSTS"
+export const FETCH_POSTS_SUCCESS = "FETCH_POSTS_SUCCESS";
+export const FETCH_POSTS_FAILURE = "FETCH_POSTS_FAILURE";
+
+//Single post
+export const FETCH_SINGLE_POST = "FETCH_SINGLE_POST";
+export const FETCH_SINGLE_POST_SUCCESS = "FETCH_SINGLE_POST_SUCCESS";
+export const FETCH_SINGLE_POST_FAILURE = "FETCH_SINGLE_POST_FAILURE";
+export const RESET_ACTIVE_POST = "RESET_ACTIVE_POST";
+
+//Delete post
+export const DELETE_POST = "DELETE_POST";
+export const DELETE_POST_SUCCESS = "DELETE_POST_SUCCESS";
+export const DELETE_POST_FAILURE = "DELETE_POST_FAILURE";
+
+//PostForm
+export const CREATE_POST = "CREATE_POST";
+export const CREATE_POST_SUCCESS = "CREATE_POST_SUCCESS";
+export const CREATE_POST_FAILURE = "CREATE_POST_FAILURE";
+
+//Login
+export const LOGIN = "LOGIN";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
+
+//Logout
+export const LOGOUT = "LOGOUT";
+
+//Approve post
+
+export const APPROVE_POST = "APPROVE_POST";
+export const APPROVE_POST_SUCCESS = "APPROVE_POST_SUCCESS";
+export const APPROVE_POST_FAILURE = "APPROVE_POST_FAILURE"
